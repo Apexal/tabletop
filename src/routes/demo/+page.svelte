41c48a6d3421919543675443e43b6576dc2f9252
Frank Matranga <thefrankmatranga@gmail.com>
2023-08-17 00:31:53 -0400

    Add grid

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -2,11 +2,13 @@
 	import Token from '$lib/Token.svelte';
 	import { supabase } from '$lib/supabaseClient.js';
 	import { onMount } from 'svelte';
-	import { Stage, Layer, Circle, type KonvaDragTransformEvent } from 'svelte-konva';
+	import { Stage, Layer, Circle, type KonvaDragTransformEvent, Line } from 'svelte-konva';
 
 	export let data;
 
-  const gridCellSize = 50;
+	const gridCellSize = 50;
+	const gridTotalHeight = gridCellSize * 20;
+	const gridTotalWidth = gridCellSize * 30;
 
 	onMount(() => {
 		const channel = supabase
@@ -50,21 +52,45 @@
 
 {#if typeof window !== 'undefined'}
 	<Stage config={{ width: window.innerWidth, height: window.innerHeight, draggable: true }}>
+		<Layer
+			config={{
+				id: 'grid',
+				draggable: false
+			}}
+		>
+			{#each { length: Math.round(gridTotalHeight / gridCellSize + 1) } as _, index}
+				<Line
+					config={{
+						stroke: 'black',
+						points: [0, index * gridCellSize, gridTotalWidth, index * gridCellSize]
+					}}
+				/>
+			{/each}
+
+			{#each { length: Math.round(gridTotalWidth / gridCellSize + 1) } as _, index}
+				<Line
+					config={{
+						stroke: 'black',
+						points: [index * gridCellSize, 0, index * gridCellSize, gridTotalHeight]
+					}}
+				/>
+			{/each}
+		</Layer>
 		<Layer>
 			{#each data.tokens as token (token.id)}
 				{#if token.x !== null && token.y !== null}
-        <Token
-          {gridCellSize}
-					config={{
-            fill: "green",
-            radius: gridCellSize,
-            x: Math.round(token.x),
-            y: Math.round(token.y),
-            draggable: true
-          }}
-          on:moveTo={(ev) => handleTokenMoveTo(token.id, ev.detail)}
-				/>          
-        {/if}
+					<Token
+						{gridCellSize}
+						config={{
+							fill: 'green',
+							radius: gridCellSize,
+							x: Math.round(token.x),
+							y: Math.round(token.y),
+							draggable: true
+						}}
+						on:moveTo={(ev) => handleTokenMoveTo(token.id, ev.detail)}
+					/>
+				{/if}
 			{/each}
 		</Layer>
 	</Stage>
